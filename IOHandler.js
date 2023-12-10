045beee7c3055e5fa81b9b5d541543b334586a6a
RyanAbinugraha <90330333+RyanAbinugraha@users.noreply.github.com>
2023-12-11 00:02:23 +0700

    Create IOHandler.js

---
+++ IOHandler.js
@@ -0,0 +1,23 @@
+// IOHandler.js
+var freeze = false;
+
+function onMouseClick(event) {
+    freeze = !freeze;
+}
+
+document.addEventListener('click', onMouseClick, false);
+
+function onKeydown(event) {
+    if (event.keyCode == 32) {
+        freeze = true;
+    }
+}
+
+function onKeyup(event) {
+    if (event.keyCode == 32) {
+        freeze = false;
+    }
+}
+
+document.addEventListener('keydown', onKeydown, false);
+document.addEventListener('keyup', onKeyup, false);
